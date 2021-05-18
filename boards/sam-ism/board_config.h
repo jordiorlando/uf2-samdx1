77a5078dd3c4c43dd76ad902d4a7ec5cd0ccc9c0
Jordi Pakey-Rodriguez <jordi@0xdec.im>
2021-05-17 21:21:56 -0700

    Add SAM-ISM

---
+++ boards/sam-ism/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "Zeroform Design"
+#define PRODUCT_NAME "SAM-ISM"
+#define VOLUME_LABEL "SAMISM_BOOT"
+#define INDEX_URL "https://zeroform.design"
+#define BOARD_ID "SAMD21E18A-SAM-ISM"
+
+#define USB_VID 0x1209
+#define USB_PID 0x9150
+
+#define LED_PIN PIN_PA00
+
+#endif
